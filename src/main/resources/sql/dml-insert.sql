d17577991f8a93b3c4d7ba89bcc32c16b5588b96
Jestersage <gleung4@gmail.com>
2015-01-28 19:11:30 -0800

    Sample structure and data

---
+++ src/main/resources/sql/dml-insert.sql
@@ -0,0 +1,40 @@
+--FOR ADDING VARIABLE RECORDS
+--
+--
+INSERT INTO employee
+  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
+VALUES
+  (124816, "gleung4@gmail.com", "George", "Leung", 03, 123456, "georgel");
+INSERT INTO Credential
+  (username, password) 
+VALUES 
+  ("georgel", "password");
+--
+--
+INSERT INTO employee
+  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
+VALUES
+  (123456, "gabeh3264@gmail.com", "Gabe Hung", "Leung", 02, 000001, "gabeh");
+INSERT INTO Credential
+  (username, password) 
+VALUES 
+  ("gabeh", "password");
+--
+--
+INSERT INTO employee
+  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
+VALUES
+  (000001, "gleung4@gmail.com", "CEO", "Admin", 01, 000001, "root");
+INSERT INTO Credential
+  (username, password) 
+VALUES 
+  ("root", "password");
+--
+--
+INSERT INTO roleAssign
+  (roleID, employeeID) 
+VALUES 
+  (1, 123456);
+
+
+
