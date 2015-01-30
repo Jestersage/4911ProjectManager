83a5f31d2c33f5be1c5f8aa65d831a6ec335da26
fred <fredxie26@gmail.com>
2015-01-29 22:28:19 -0800

    employee entities

--- src/main/java/ca/bcit/info/pms/model/Employee.java
+++ src/main/java/ca/bcit/info/pms/model/Employee.java
@@ -12,73 +12,132 @@ import java.lang.Override;
 
 @Entity
 @Table(name = "Employee")
-public class Employee implements Serializable
-{
+public class Employee implements Serializable {
 
-   @Id
-   @Column(name = "id", updatable = false, nullable = false)
-   private Long id;
-   @Version
-   @Column(name = "version")
-   private int version;
+	@Id
+	@Column(name = "id", updatable = false, nullable = false)
+	private String id;
+	@Version
+	@Column(name = "version")
+	private int version;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "payGrade")
+	private double payGrade;
 
-   public Long getId()
-   {
-      return this.id;
-   }
+	public String getEmail() {
+		return email;
+	}
 
-   public void setId(final Long id)
-   {
-      this.id = id;
-   }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-   public int getVersion()
-   {
-      return this.version;
-   }
+	public String getFirstName() {
+		return firstName;
+	}
 
-   public void setVersion(final int version)
-   {
-      this.version = version;
-   }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
-   }
+	public String getLastName() {
+		return lastName;
+	}
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-      {
-         return true;
-      }
-      if (!(obj instanceof Employee))
-      {
-         return false;
-      }
-      Employee other = (Employee) obj;
-      if (id != null)
-      {
-         if (!id.equals(other.id))
-         {
-            return false;
-         }
-      }
-      return true;
-   }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-   @Override
-   public int hashCode()
-   {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((id == null) ? 0 : id.hashCode());
-      return result;
-   }
+	public double getPayGrade() {
+		return payGrade;
+	}
+
+	public void setPayGrade(double payGrade) {
+		this.payGrade = payGrade;
+	}
+
+	public String getSupervisorID() {
+		return supervisorID;
+	}
+
+	public void setSupervisorID(String supervisorID) {
+		this.supervisorID = supervisorID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getActiveStatus() {
+		return activeStatus;
+	}
+
+	public void setActiveStatus(int activeStatus) {
+		this.activeStatus = activeStatus;
+	}
+
+	@Column(name = "supervisorID")
+	private String supervisorID;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "activeStatus")
+	private int activeStatus;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (id != null)
+			result += "id: " + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Employee)) {
+			return false;
+		}
+		Employee other = (Employee) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 }
