fab09f7e287a210d2f9db71d09072c5e003865c4
Jestersage <gleung4@gmail.com>
2015-01-31 20:44:33 -0800

    quick update of the db

--- src/main/resources/sql/dml.sql
+++ src/main/resources/sql/dml.sql
@@ -15,3 +15,35 @@ SELECT roleID, name
 SELECT employeeID, username, email, firstName, lastName, payGrade, supervisorID, active 
   FROM employee;
 --
+-- UPDATE / U
+UPDATE Credential SET password='P@$$w0rd' 
+    WHERE username = 'georgel';
+--
+UPDATE roleAssign SET roleID = 1
+WHERE
+  employeeID = 124816;
+--
+-- feel free to add the right field
+UPDATE employee SET 
+  email = 'gleung4@hotmail.com', 
+  firstName = 'George gh', 
+  lastName = 'Leung', 
+  supervisorID = 000001, 
+  payGrade = 1,
+  active = 0 
+WHERE
+  employeeID = 124816;
+--
+--
+--DELETE -- Variables only
+--Order: deelete roleAssign, SELECT username
+DELETE from roleAssign
+  WHERE employeeID = 124816;
+SELECT username
+  FROM employee 
+  WHERE employeeID = 124816;
+--store username
+DELETE from employee
+  WHERE employeeID = 124816;
+DELETE from Credential
+  WHERE username = 'georgel';
