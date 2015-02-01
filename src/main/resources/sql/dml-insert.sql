30614f5ab9be4402c8f6c73de19c0739193343f4
Jestersage <gleung4@gmail.com>
2015-01-31 17:28:25 -0800

    quick update of the db

--- src/main/resources/sql/dml-insert.sql
+++ src/main/resources/sql/dml-insert.sql
@@ -1,37 +1,44 @@
---FOR ADDING VARIABLE RECORDS
+-- INSERT DATA
+-- Order MUST BE: Credentials, Then employee, then roleAssign
 --
 --
-INSERT INTO employee
-  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
-VALUES
-  (124816, "gleung4@gmail.com", "George", "Leung", 03, 123456, "georgel");
-INSERT INTO Credential
-  (username, password) 
-VALUES 
-  ("georgel", "password");
---
---
-INSERT INTO employee
-  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
-VALUES
-  (123456, "gabeh3264@gmail.com", "Gabe Hung", "Leung", 02, 000001, "gabeh");
-INSERT INTO Credential
-  (username, password) 
-VALUES 
-  ("gabeh", "password");
---
---
-INSERT INTO employee
-  (employeeID, email, firstName, lastName, payGrade, supervisorID, username) 
-VALUES
-  (000001, "gleung4@gmail.com", "CEO", "Admin", 01, 000001, "root");
 INSERT INTO Credential
   (username, password) 
 VALUES 
   ("root", "password");
+INSERT INTO employee
+  (employeeID, username, email, firstName, lastName, payGrade, supervisorID, active)
+VALUES 
+  (000001, "root", "gleung4@gmail.com", "CEO", "Admin", 01, 000001, 1 );
+INSERT INTO roleAssign
+  (roleID, employeeID) 
+VALUES 
+  (1, 000001);
 --
 --
+INSERT INTO Credential
+  (username, password) 
+VALUES 
+  ("gabeh", "password");
+INSERT INTO employee
+  (employeeID, username, email, firstName, lastName, payGrade, supervisorID, active)
+VALUES
+  (123456, "gabeh", "gabeh3264@gmail.com", "Gabe", "Hung", 02, 000001, 1);
 INSERT INTO roleAssign
   (roleID, employeeID) 
 VALUES 
   (1, 123456);
+--
+--
+INSERT INTO Credential
+  (username, password) 
+VALUES 
+  ("georgel", "password");
+INSERT INTO employee
+  (employeeID, username, email, firstName, lastName, payGrade, supervisorID, active)
+VALUES
+  (124816, "georgel", "gleung4@gmail.com", "George", "Leung", 03, 123456, 1);
+INSERT INTO roleAssign
+  (roleID, employeeID) 
+VALUES 
+  (2, 124816);
