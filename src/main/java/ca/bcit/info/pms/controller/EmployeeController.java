dcfa38ba00ac293ae7e21e82033607a34371a41a
fred <fredxie26@gmail.com>
2015-02-02 14:34:58 -0800

    Add new employee implementation

--- src/main/java/ca/bcit/info/pms/controller/EmployeeController.java
+++ src/main/java/ca/bcit/info/pms/controller/EmployeeController.java
@@ -2,19 +2,23 @@ package ca.bcit.info.pms.controller;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import ca.bcit.info.pms.access.EmployeeManager;
 import ca.bcit.info.pms.model.Credentials;
 import ca.bcit.info.pms.model.Employee;
 
+@Named("empController")
+@SessionScoped	
 public class EmployeeController implements Serializable {
 	@Inject
 	private EmployeeManager empManager;
 	/**
 	 * Hold the new employee information
 	 */
-	private Employee newEmp;
+	private Employee newEmp = new Employee("A00123456", "aa@bb.cc", "fred", "xie", 11.11, "a12345678", "fredxie", 1);
 	/**
 	 * Hold the new Credential information
 	 */
