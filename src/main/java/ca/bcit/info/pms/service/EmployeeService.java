8840fe983083bbdc2c285283d5b473698a9f9e0d
huanan <birch0930@gmail.com>
2015-02-11 02:05:15 -0800

    update openshift

--- src/main/java/ca/bcit/info/pms/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package ca.bcit.info.pms.service;
-
-import java.io.Serializable;
-
-import ca.bcit.info.pms.model.Credentials;
-
-public interface EmployeeService {
-	boolean checkCredentials(Credentials credentials);
-}
