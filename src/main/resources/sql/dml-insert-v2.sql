468a68e397ebe6b8a58e927d205f9c4ce6f119f2
Jestersage <gleung4@gmail.com>
2015-02-17 14:43:16 -0800

    db insert updated

--- src/main/resources/sql/dml-insert-v2.sql
+++ src/main/resources/sql/dml-insert-v2.sql
@@ -3,31 +3,39 @@
 --
 --
 INSERT INTO Credentials
-  (username, password, salt) 
+  (username, password) 
 VALUES 
-  ("root", "password", "");
-INSERT INTO employee
-  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active, role) 
+  ("root", "password");
+INSERT INTO Employee
+  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active) 
 VALUES 
-  (000001, "root", "gleung4@gmail.com", "CEO", "Admin", 01, 000001, 1, 1 );
+  (000001, "root", "gleung4@gmail.com", "CEO", "Admin", 01, 000001, 1 );
+INSERT INTO HR
+  (employeeID)
+VALUES
+  (000001);
 --
 --
 INSERT INTO Credentials
-  (username, password, salt) 
+  (username, password) 
 VALUES 
-  ("gabeh", "password", "");
+  ("gabeh", "password");
 INSERT INTO employee
-  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active, role) 
+  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active) 
 VALUES
-  (123456, "gabeh", "gabeh3264@gmail.com", "Gabe", "Hung", 02, 000001, 1, 1);
+  (123456, "gabeh", "gabeh3264@gmail.com", "Gabe", "Hung", 02, 000001, 1);
+INSERT INTO HR
+  (employeeID)
+VALUES
+  (123456);
 --
 --
 INSERT INTO Credentials
-  (username, password, salt) 
+  (username, password) 
 VALUES 
-  ("georgel", "password", "");
+  ("georgel", "password");
 INSERT INTO employee
-  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active, role) 
+  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active) 
 VALUES
-  (124816, "georgel", "gleung4@gmail.com", "George", "Leung", 03, 123456, 1, 2);
+  (124816, "georgel", "gleung4@gmail.com", "George", "Leung", 03, 123456, 1);
   
