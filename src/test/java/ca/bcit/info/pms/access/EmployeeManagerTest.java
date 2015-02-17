7dfad23801f9bbc2cb559e8354b3be0c3d241ee9
Huck5 <fcharleslundaahl@gmail.com>
2015-02-16 22:31:27 -0800

    EmployeeManager test

--- src/test/java/ca/bcit/info/pms/access/EmployeeManagerTest.java
+++ src/test/java/ca/bcit/info/pms/access/EmployeeManagerTest.java
@@ -1,5 +1,10 @@
 package ca.bcit.info.pms.access;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +12,8 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import ca.bcit.info.pms.model.Credential;
@@ -21,6 +28,7 @@ public class EmployeeManagerTest {
     @Test
     public void should_get_all_credentials() {
         List<Credential> creds = new ArrayList<Credential>();
+        List<Credential> credsB = new ArrayList<Credential>();
         
         Credential a = new Credential("a", "a");
         Credential b = new Credential("b", "b");
@@ -30,9 +38,11 @@ public class EmployeeManagerTest {
         creds.add(b);
         creds.add(c);
         
-        empMgr.persistCredential(a);
-        empMgr.persistCredential(b);
-        empMgr.persistCredential(c);
+        credsB.add(a);
+        credsB.add(b);
+        credsB.add(c);
+        
+        assertEquals(creds, credsB);
     }
 
 }
