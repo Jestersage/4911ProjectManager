8d905b236c36849221ea98e4ec06d2187b805464
Huck5 <fcharleslundaahl@gmail.com>
2015-02-16 22:11:51 -0800

    EmployeeManagerTest

--- src/test/java/ca/bcit/info/pms/access/EmployeeManagerTest.java
+++ src/test/java/ca/bcit/info/pms/access/EmployeeManagerTest.java
@@ -1,5 +1,38 @@
 package ca.bcit.info.pms.access;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.Test;
+
+import ca.bcit.info.pms.model.Credential;
+
 public class EmployeeManagerTest {
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Inject
+    EmployeeManager empMgr;
+    
+    @Test
+    public void should_get_all_credentials() {
+        List<Credential> creds = new ArrayList<Credential>();
+        
+        Credential a = new Credential("a", "a");
+        Credential b = new Credential("b", "b");
+        Credential c = new Credential("c", "c");
+        
+        creds.add(a);
+        creds.add(b);
+        creds.add(c);
+        
+        empMgr.persistCredential(a);
+        empMgr.persistCredential(b);
+        empMgr.persistCredential(c);
+    }
 
 }
