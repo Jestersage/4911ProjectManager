034570154b5cb89a8a2cd16221a4799b085cb069
Huck5 <fcharleslundaahl@gmail.com>
2015-02-16 20:59:00 -0800

    PasswordHash test cases

--- src/test/java/ca/bcit/info/pms/service/PasswordHashTest.java
+++ src/test/java/ca/bcit/info/pms/service/PasswordHashTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
