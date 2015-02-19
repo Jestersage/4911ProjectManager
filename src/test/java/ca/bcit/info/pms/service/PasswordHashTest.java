201d7eb87e903b95c314ec7b9ee5d189aa71f560
Huck5 <fcharleslundaahl@gmail.com>
2015-02-18 19:42:25 -0800

    Added "beans.xml" to test jar

--- src/test/java/ca/bcit/info/pms/service/PasswordHashTest.java
+++ src/test/java/ca/bcit/info/pms/service/PasswordHashTest.java
@@ -7,9 +7,11 @@ import static org.junit.Assert.assertTrue;
 
 
 
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 //  import org.jboss.arquillian.container.test.api.Deployment;
 //  import org.jboss.arquillian.junit.Arquillian;
@@ -24,7 +26,8 @@ public class PasswordHashTest {
     @Deployment
     public static JavaArchive createDeployment() {
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
-                .addClass(PasswordHash.class);
+                .addClass(PasswordHash.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
         return jar;
     }
 
