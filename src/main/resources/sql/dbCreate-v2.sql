dd177f4f7b8b2cf66287a5060c2d5fe0f03c5194
Huck5 <fcharleslundaahl@gmail.com>
2015-03-01 14:26:49 -0800

    sd

---
+++ src/main/resources/sql/dbCreate-v2.sql
@@ -0,0 +1,165 @@
+DROP DATABASE pms;
+CREATE DATABASE pms;
+--
+GRANT ALL PRIVILEGES ON pms.* TO atdefault@localhost IDENTIFIED BY 'atdefault';
+GRANT ALL PRIVILEGES ON pms.* TO atdefault@"%" IDENTIFIED BY 'atdefault';
+--
+use pms;
+--
+ALTER TABLE employee DROP FOREIGN KEY FKemployeePG;
+ALTER TABLE employee DROP FOREIGN KEY FKemployeeCred;
+ALTER TABLE Timesheet DROP FOREIGN KEY FKTimesheetEmp;
+ALTER TABLE TimesheetRow DROP FOREIGN KEY FKTimesheetRow;
+ALTER TABLE TimesheetRow DROP FOREIGN KEY FKTimesheetRowWP;
+ALTER TABLE WorkPackage DROP FOREIGN KEY FKWPProject;
+ALTER TABLE Report DROP FOREIGN KEY FKReportWP;
+DROP TABLE IF EXISTS Report;
+DROP TABLE IF EXISTS TimesheetRow;
+DROP TABLE IF EXISTS Timesheet;
+DROP TABLE IF EXISTS WorkPackage;
+DROP TABLE IF EXISTS Project;
+DROP TABLE IF EXISTS Credentials;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Signatures;
+DROP TABLE IF EXISTS HR;
+--
+CREATE TABLE Report (
+  reportID varchar(20) NOT NULL,
+  packageID int(10) NOT NULL,
+  CONSTRAINT reportID 
+    PRIMARY KEY (reportID));
+--
+CREATE TABLE WorkPackage (
+  packageID    int(10) NOT NULL AUTO_INCREMENT,
+  projectID    varchar(20) NOT NULL,
+  packageNum   varchar(20) NOT NULL,
+  wpEmployeeID varchar(10) NOT NULL,
+  estimateCost numeric(20, 4), 
+  actualCost   numeric(20, 4), 
+  parentwpID   int(10),
+  status       tinyint(1),
+  CONSTRAINT packageID
+    PRIMARY KEY (packageID));
+--
+CREATE TABLE Project (
+  projectID    varchar(20) NOT NULL, 
+  projectName  varchar(255) NOT NULL, 
+  description  varchar(255) NOT NULL, 
+  startDate    date NOT NULL, 
+  endDate      date, 
+<<<<<<< HEAD
+  budget       numeric(20, 4), 
+  status       tinyint(1), 
+  pmEmployeeID    varchar(10),
+=======
+  budget       numeric(20, 4),
+  status       tinyint(1),
+  pmEmployeeID varchar(10),
+>>>>>>> 100a950d632b66ed06902b9f38b0dc55ddc3bb87
+  genReport       tinyint(1) NOT NULL,
+  CONSTRAINT projectID  
+    PRIMARY KEY (projectID)
+);
+--
+--
+CREATE TABLE TimesheetRow (
+  timesheetID    int(10), 
+  timesheetrowID int(10) NOT NULL, 
+  projectID      varchar(20) NOT NULL, 
+  packageID      int(10) NOT NULL, 
+  weekending     date NOT NULL, 
+  total          numeric(20, 4) NOT NULL, 
+  notes          varchar(255) NOT NULL, 
+  sunday         numeric(2, 2) DEFAULT 0, 
+  monday         numeric(2, 2) DEFAULT 0, 
+  tuesday        numeric(2, 2) DEFAULT 0, 
+  wednesday      numeric(2, 2) DEFAULT 0, 
+  thursday       numeric(2, 2) DEFAULT 0, 
+  friday         numeric(2, 2) DEFAULT 0, 
+  saturday       numeric(2, 2) DEFAULT 0, 
+  CONSTRAINT timesheetrowID 
+    PRIMARY KEY (timesheetrowID));
+--
+CREATE TABLE Timesheet (
+  timesheetID int(10) NOT NULL AUTO_INCREMENT, 
+  employeeID  varchar(10) NOT NULL, 
+  weeknumber  int(2) NOT NULL, 
+  weekending  date NOT NULL, 
+  grandTotal  numeric(2, 2) NOT NULL, 
+  overtime    numeric(2, 2), 
+  flexTime    numeric(2, 2), 
+  signed      varchar(255), 
+  approved    varchar(255),
+  signID      int(10),
+  CONSTRAINT timesheetID 
+    PRIMARY KEY (timesheetID));
+--
+CREATE TABLE Signature(
+    signId    int(10) NOT NULL PRIMARY KEY ,
+    signature TINYBLOB NOT NULL, 
+    publicKey BLOB NOT NULL
+);
+-- salt need to be as long as hashed password
+CREATE TABLE Credentials (
+  username varchar(255) NOT NULL, 
+  password varchar(512) NOT NULL,
+  CONSTRAINT username
+    PRIMARY KEY (username));
+--
+CREATE TABLE Employee (
+  employeeID   varchar(10) NOT NULL, 
+  username     varchar(255) NOT NULL, 
+  email        varchar(255) NOT NULL, 
+  firstName    varchar(255) NOT NULL, 
+  lastName     varchar(255) NOT NULL, 
+  paygradeID   varchar(10) NOT NULL, 
+  supervisorID varchar(10),
+  active       bool NOT NULL,
+  vacationTime    numeric(2,2),
+  flexTime        numeric(2, 2),
+  CONSTRAINT employeeID
+    PRIMARY KEY (employeeID)
+);
+--
+CREATE TABLE HR(
+    employeeID varchar(10) NOT NULL,
+    PRIMARY KEY (employeeID)
+);
+--
+CREATE TABLE PayGrade(
+    paygradeID varchar(2)   NOT NULL,
+    cost       numeric(4,2) NOT NULL,
+    PRIMARY KEY (paygradeID)
+);
+--
+CREATE TABLE ProjectAssignment(
+    projectID    varchar(20) NOT NULL, 
+    employeeID   varchar(10) NOT NULL, 
+    PRIMARY KEY (projectID, employeeID)
+);
+--
+CREATE TABLE WorkAssignment(
+    packageID    int(10)     NOT NULL, 
+    employeeID   varchar(10) NOT NULL, 
+    PRIMARY KEY (packageID, employeeID)
+);
+--
+ALTER TABLE Report ADD CONSTRAINT FKReportWP FOREIGN KEY (packageID) REFERENCES WorkPackage (packageID);
+--
+ALTER TABLE WorkPackage ADD CONSTRAINT FKSubWorkPackage FOREIGN KEY (parentwpID) REFERENCES WorkPackage (packageID);
+ALTER TABLE WorkPackage ADD CONSTRAINT FKWPProject FOREIGN KEY (projectID) REFERENCES Project (projectID);
+--
+ALTER TABLE TimesheetRow  ADD CONSTRAINT FKTimesheetRow FOREIGN KEY (timesheetID) REFERENCES Timesheet (timesheetID);
+ALTER TABLE TimesheetRow ADD CONSTRAINT FKTimesheetRowWP FOREIGN KEY (packageID) REFERENCES WorkPackage (packageID);
+ALTER TABLE Timesheet ADD CONSTRAINT FKTimesheetEmp FOREIGN KEY (employeeID) REFERENCES employee (employeeID);
+ALTER TABLE Timesheet ADD CONSTRAINT FKsignature FOREIGN KEY (signID) REFERENCES Signature (signID);
+--
+ALTER TABLE Employee ADD CONSTRAINT FKSupervisor FOREIGN KEY (supervisorID) REFERENCES Employee (employeeID);
+ALTER TABLE Employee ADD CONSTRAINT FKemployeeCred FOREIGN KEY (username) REFERENCES Credentials (username);
+ALTER TABLE HR ADD CONSTRAINT FKHR FOREIGN KEY (employeeID) REFERENCES Employee (employeeID);
+--
+ALTER TABLE ProjectAssignment ADD CONSTRAINT FKPAPro FOREIGN KEY (projectID) REFERENCES Project (projectID);
+ALTER TABLE ProjectAssignment ADD CONSTRAINT FKPAEmp FOREIGN KEY (employeeID) REFERENCES Employee (employeeID);
+--
+ALTER TABLE WorkAssignment ADD CONSTRAINT FKWAWork FOREIGN KEY (packageID) REFERENCES WorkPackage (packageID);
+ALTER TABLE WorkAssignment ADD CONSTRAINT FKWAEmp FOREIGN KEY (employeeID) REFERENCES Employee (employeeID);
---
+++ src/main/resources/sql/dml-insert-v2.sql
@@ -0,0 +1,174 @@
+-- INSERT DATA
+-- Order MUST BE: Credentials, Then employee, then roleAssign
+--
+--
+INSERT INTO Credentials
+  (username, password) 
+VALUES 
+  ("root", "password");
+INSERT INTO Employee
+  (employeeID, username, email, firstName, lastName, paygradeID, active) 
+VALUES 
+  (000001, "root", "gleung4@gmail.com", "CEO", "Admin", 01, 1 );
+INSERT INTO HR
+  (employeeID)
+VALUES
+  (000001);
+--
+--
+INSERT INTO Credentials
+  (username, password) 
+VALUES 
+  ("gabeh", "password");
+INSERT INTO Employee
+  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active) 
+VALUES
+  (123456, "gabeh", "gabeh3264@gmail.com", "Gabe", "Hung", 02, 000001, 1);
+INSERT INTO HR
+  (employeeID)
+VALUES
+  (123456);
+--
+--
+INSERT INTO Credentials
+  (username, password) 
+VALUES 
+  ("georgel", "password");
+INSERT INTO Employee
+  (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active) 
+VALUES
+  (124816, "georgel", "gleung4@gmail.com", "George", "Leung", 03, 123456, 1);
+----------------------------
+-- Sample Employee data
+---------------------------- 
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("bobDole", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000002, "bobDole", "bdole@email.com", "Bob", "Dole", 01, 000001, 1);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("fredBarns", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000003, "fredBarns", "fredb@email.com", "Frederic", "Barns", 03, 000001, 1);
+
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("rBurns", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000004, "rBurns", "bdole@email.com", "Robet", "Dolenson", 01, 000001, 1);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("hSimpson", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000005, "hSimpson", "homerS@email.com", "Homer", "Simpson", 03, 000004, 1);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("magSimpson", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000006, "magSimpson", "magS@email.com", "Maggie", "Simpson", 02, 000004, 0);
+    
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("margSimpson", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000007, "margSimpson", "margS@email.com", "Marge", "Simpson", 01, 000004, 0);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("bSimpson", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000008, "bSimpson", "bartS@email.com", "Bart", "Simpson", 03, 000005, 1);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("fArbuckle", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000009, "fArbuckle", "fredA@email.com", "Fred", "Arbuckle", 02, 000004, 1);    
+
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("garfield", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000010, "garfield", "garf@email.com", "Garfield", "Arbuckle", 03, 000009, 1);
+    
+INSERT INTO Credentials
+    (username, password)
+VALUES
+    ("odie", "password");
+INSERT INTO Employee
+    (employeeID, username, email, firstName, lastName, paygradeID, supervisorID, active)
+VALUES
+    (000011, "odie", "odie@email.com", "Odie", "Arbuckle", 02, 000009, 1);
+
+    
+
+--
+INSERT INTO Project
+  (projectID, projectName, description, startDate, status, pmEmployeeID, genReport)
+VALUES
+  (010, "Admin", "Administrative items.", "2015-01-01", 1, 000001, FALSE);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID)
+VALUES
+  (1, 010, "SICK", 000001);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID)
+VALUES
+  (2, 010, "VACN", 000001);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID)
+VALUES
+  (3, 010, "SHOL", 000001);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID)
+VALUES
+  (4, 010, "FLEX", 000001);
+--
+INSERT INTO Project
+(projectID, projectName, description, startDate, endDate, budget, status, pmEmployeeID, genReport)
+VALUES
+  (1202, "BlackBerry", "BlackBerry was formally know as RIM.", "2015-02-15", "2215-08-30", 10000, 1, 123456, TRUE);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID)
+VALUES
+  (5, 1202, "AB", 123456);
+INSERT INTO WorkPackage
+  (packageID, projectID, packageNum, wpEmployeeID, estimateCost, actualCost, parentwpID, status)
+VALUES
+  (6, 1202, "AB-1", 123456, 5000, 5000, 5, 2);
+INSERT INTO WorkPackage
+(packageID, projectID, packageNum, wpEmployeeID, estimateCost, actualCost, parentwpID, status)
+VALUES
+  (7, 1202, "AB-2", 123456, 5000, 4500, 5, 1);
