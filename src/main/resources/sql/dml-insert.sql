befed5016e3eb47e765e2390dc1171bf4b07ac39
Jestersage <gleung4@gmail.com>
2015-03-03 00:11:50 -0800

    fixed dml

--- src/main/resources/sql/dml-insert.sql
+++ src/main/resources/sql/dml-insert.sql
@@ -132,40 +132,41 @@ INSERT INTO Employee
 VALUES
     (000011, "odie", "odie@email.com", "Odie", "Arbuckle", 02, 000009, 1);
 --
+--
 INSERT INTO Project
-  (projectID, projectName, description, startDate, status, pmEmployeeID, genReport)
+  (projectID, projectName, description, startDate, status, employeeID, genReport)
 VALUES
   (010, "Admin", "Administrative items.", "2015-01-01", 1, 000001, FALSE);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID)
+  (packageID, projectID, packageNum, employeeID)
 VALUES
   (1, 010, "SICK", 000001);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID)
+(packageID, projectID, packageNum, employeeID)
 VALUES
   (2, 010, "VACN", 000001);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID)
+  (packageID, projectID, packageNum, employeeID)
 VALUES
   (3, 010, "SHOL", 000001);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID)
+  (packageID, projectID, packageNum, employeeID)
 VALUES
   (4, 010, "FLEX", 000001);
 --
 INSERT INTO Project
-(projectID, projectName, description, startDate, endDate, budget, status, pmEmployeeID, genReport)
+(projectID, projectName, description, startDate, endDate, budget, status, employeeID, genReport)
 VALUES
   (1202, "BlackBerry", "BlackBerry was formally know as RIM.", "2015-02-15", "2215-08-30", 10000, 1, 123456, TRUE);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID)
+  (packageID, projectID, packageNum, employeeID)
 VALUES
   (5, 1202, "AB", 123456);
 INSERT INTO WorkPackage
-  (packageID, projectID, packageNum, wpEmployeeID, estimateCost, actualCost, parentwpID, status)
+  (packageID, projectID, packageNum, employeeID, estimateCost, actualCost, parentwpID, status)
 VALUES
   (6, 1202, "AB-1", 123456, 5000, 5000, 5, 2);
 INSERT INTO WorkPackage
-(packageID, projectID, packageNum, wpEmployeeID, estimateCost, actualCost, parentwpID, status)
+(packageID, projectID, packageNum, employeeID, estimateCost, actualCost, parentwpID, status)
 VALUES
   (7, 1202, "AB-2", 123456, 5000, 4500, 5, 1);
