e40db45a3975763a778a8389b6015e6c85a7c7f8
prestonjang1520 <prestonjang1520@gmail.com>
2015-03-11 15:01:08 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/webapp/employeeBootstrap/newProject.xhtml
    
    Revert "Created ProjectManager and Services classes"
    
    This reverts
    commit d77e8b944ca7c50c8034f833489617792e3117f2.

--- src/main/java/ca/bcit/info/pms/access/ProjectManager.java
+++
@@ -1,5 +0,0 @@
-package ca.bcit.info.pms.access;
-
-public class ProjectManager {
-
-}
--- src/main/java/ca/bcit/info/pms/service/ProjectService.java
+++ src/main/java/ca/bcit/info/pms/service/ProjectService.java
@@ -1,16 +1,7 @@
 package ca.bcit.info.pms.service;
 
-import java.util.List;
-
-import ca.bcit.info.pms.model.Project;
-
 public interface ProjectService {
 
-    void persistProject(Project newProject);
-    
-    void updateProject(Project newProject);
-    
-    List<Project> getAllProjects();
-    
-    Project findProjectByProjectName(final String projectName);
+    void persistProject();
+    void updateProject();
 }
