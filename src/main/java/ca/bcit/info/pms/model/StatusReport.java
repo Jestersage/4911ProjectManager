112cd2706d140a55d1950c0e675448815c0aec65
angela <starangelam@gmail.com>
2015-03-11 14:10:47 -0700

    add statusReport entity

---
+++ src/main/java/ca/bcit/info/pms/model/StatusReport.java
@@ -0,0 +1,75 @@
+package ca.bcit.info.pms.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+/**
+ * Status Report.
+ */
+@Entity
+public class StatusReport
+{
+    @Id
+    @Column(name = "reportID", updatable = false, nullable = false)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "packageID")
+    private WorkPackage workPackage;
+
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public WorkPackage getWorkPackage() {
+        return workPackage;
+    }
+
+    public void setWorkPackage(WorkPackage workPackage) {
+        this.workPackage = workPackage;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StatusReport)) return false;
+
+        StatusReport that = (StatusReport) o;
+
+        if (id != that.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("StatusReport{");
+        sb.append("workPackage=").append(workPackage.getProject().getId())
+                .append("-")
+                .append(workPackage.getPackageNum());
+        sb.append('}');
+        return sb.toString();
+    }
+}
