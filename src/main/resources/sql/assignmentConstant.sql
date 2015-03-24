96d68a0fa8cc0eb8d53ab73aa8d76c16b38eab5a
Jestersage <gleung4@gmail.com>
2015-03-24 15:17:27 -0700

    Update db

---
+++ src/main/resources/sql/assignmentConstant.sql
@@ -0,0 +1,94 @@
+-- TODO:
+-- assign all employee to the project 010
+-- assign all employee to the respective workpackage
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000001);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,123456);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,124816);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000002);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000003);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000004);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000005);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000006);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000007);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000008);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000009);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000010);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,000011);
+--
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,333);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,555);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,777);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,888);
+--
+INSERT INTO ProjectAssignment
+    (projectID, employeeID)
+VALUES
+    (010,999);
+--
