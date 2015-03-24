93fe9c11cb88a0f651b716ed7a2dcbe75856c868
Jestersage <gleung4@gmail.com>
2015-03-24 15:46:10 -0700

    Update db

--- src/main/resources/sql/assignmentConstant.sql
+++ src/main/resources/sql/assignmentConstant.sql
@@ -6,65 +6,299 @@ INSERT INTO ProjectAssignment
 VALUES
     (010,000001);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000001);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000001);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000001);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000001);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,123456);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,123456);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,123456);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,123456);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,123456);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,124816);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,124816);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,124816);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,124816);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,124816);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000002);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000002);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000002);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000002);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000002);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000003);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000003);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000003);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000003);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000003);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000004);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000004);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000004);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000004);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000004);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000005);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000005);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000005);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000005);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000005);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000006);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000006);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000006);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000006);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000006);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000007);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000007);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000007);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000007);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000007);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000008);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000008);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000008);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000008);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000008);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000009);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000009);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000009);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000009);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000009);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000010);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000010);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000010);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000010);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000010);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,000011);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,000011);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,000011);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,000011);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,000011);
+--
 --
 --
 INSERT INTO ProjectAssignment
@@ -72,23 +306,115 @@ INSERT INTO ProjectAssignment
 VALUES
     (010,333);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,333);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,333);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,333);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,333);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,555);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,555);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,555);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,555);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,555);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,777);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,777);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,777);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,777);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,777);
+--
+--
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,888);
+--
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,888);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,888);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,888);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,888);
+--
 --
 INSERT INTO ProjectAssignment
     (projectID, employeeID)
 VALUES
     (010,999);
 --
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (1,999);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (2,999);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (3,999);
+INSERT INTO WorkAssignment
+    (packageID, employeeID)
+VALUES
+    (4,999);
+--
+--
+--
+-- Package assignment constant
