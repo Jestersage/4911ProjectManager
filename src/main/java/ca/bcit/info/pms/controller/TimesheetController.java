1c138ecfe662d9d524a35bc08e81170887c961e0
Jestersage <gleung4@gmail.com>
2015-03-24 14:13:06 -0700

    begining timesheetManager

--- src/main/java/ca/bcit/info/pms/controller/TimesheetController.java
+++ src/main/java/ca/bcit/info/pms/controller/TimesheetController.java
@@ -3,9 +3,12 @@ package ca.bcit.info.pms.controller;
 import java.io.Serializable;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import ca.bcit.info.pms.service.TimesheetService;
 
+@Named( "timesheetController" )
+@RequestScoped
 public class TimesheetController implements Serializable {
 
     @Inject
