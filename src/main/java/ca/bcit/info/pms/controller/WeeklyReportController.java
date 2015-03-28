c375f9d6edba04e2daacb7840e5bb5aec96c6315
rafaelrozon <rafaelrozon@gmail.com>
2015-03-28 13:36:16 -0700

    summary table in status report connected to db. Needs more tests.

--- src/main/java/ca/bcit/info/pms/controller/WeeklyReportController.java
+++ src/main/java/ca/bcit/info/pms/controller/WeeklyReportController.java
@@ -1,6 +1,7 @@
 package ca.bcit.info.pms.controller;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -8,6 +9,7 @@ import javax.inject.Named;
 
 import ca.bcit.info.pms.access.EngineerBudgetManager;
 import ca.bcit.info.pms.access.StatusReportManager;
+import ca.bcit.info.pms.access.TimesheetRowManager;
 import ca.bcit.info.pms.access.WorkPackageManager;
 import ca.bcit.info.pms.model.EngineerBudget;
 import ca.bcit.info.pms.model.StatusReport;
@@ -31,10 +33,13 @@ public class WeeklyReportController implements Serializable
 	private StatusReportManager statusReportMngr;
 
 	@Inject
-	private WorkPackage wp;
+	private EngineerBudgetManager engineerBudgetMngr;
 
 	@Inject
-	private EngineerBudgetManager engineerBudgetMngr;
+	private TimesheetRowManager timeSheetRowMngr;
+
+	@Inject
+	private WorkPackage wp;
 
 	@Inject
 	private EngineerBudget engineerBudget;
@@ -42,8 +47,19 @@ public class WeeklyReportController implements Serializable
 	@Inject
 	private StatusReport statusReport;
 
+	Map< String, Double > cost;
+
+	private Double p1;
+	private Double p2;
+	private Double p3;
+	private Double p4;
+	private Double p5;
+	private Double p6;
+	private Double ss;
+	private Double js;
+	private Double ds;
+
 	private int id;
-	private int prev;
 	private String name;
 
 	public WeeklyReportController()
@@ -80,6 +96,20 @@ public class WeeklyReportController implements Serializable
 			engineerBudget.setDS( budget.getDS() );
 			engineerBudget.setJS( budget.getJS() );
 			engineerBudget.setSS( budget.getSS() );
+
+			cost = timeSheetRowMngr.getManHoursPerPayLevel( id );
+
+			setP1( cost.get( "P1" ).doubleValue() );
+			setP2( cost.get( "P2" ).doubleValue() );
+			setP3( cost.get( "P3" ).doubleValue() );
+			setP4( cost.get( "P4" ).doubleValue() );
+			setP5( cost.get( "P5" ).doubleValue() );
+			setP6( cost.get( "P6" ).doubleValue() );
+			setSs( cost.get( "SS" ).doubleValue() );
+			setJs( cost.get( "JS" ).doubleValue() );
+			setDs( cost.get( "DS" ).doubleValue() );
+			System.out.println( "cost = " + cost.toString() );
+
 		} else
 		{
 			setName( "" );
@@ -152,6 +182,96 @@ public class WeeklyReportController implements Serializable
 	{
 		this.engineerBudget = engineerBudget;
 
+	}
+
+	public Double getP1()
+	{
+		return p1;
+	}
+
+	public void setP1( Double p1 )
+	{
+		this.p1 = p1;
+	}
+
+	public Double getP2()
+	{
+		return p2;
+	}
+
+	public void setP2( Double p2 )
+	{
+		this.p2 = p2;
+	}
+
+	public Double getP3()
+	{
+		return p3;
+	}
+
+	public void setP3( Double p3 )
+	{
+		this.p3 = p3;
+	}
+
+	public Double getP4()
+	{
+		return p4;
+	}
+
+	public void setP4( Double p4 )
+	{
+		this.p4 = p4;
+	}
+
+	public Double getP5()
+	{
+		return p5;
+	}
+
+	public void setP5( Double p5 )
+	{
+		this.p5 = p5;
+	}
+
+	public Double getP6()
+	{
+		return p6;
+	}
+
+	public void setP6( Double p6 )
+	{
+		this.p6 = p6;
+	}
+
+	public Double getSs()
+	{
+		return ss;
+	}
+
+	public void setSs( Double ss )
+	{
+		this.ss = ss;
+	}
+
+	public Double getJs()
+	{
+		return js;
+	}
+
+	public void setJs( Double js )
+	{
+		this.js = js;
+	}
+
+	public Double getDs()
+	{
+		return ds;
+	}
+
+	public void setDs( Double ds )
+	{
+		this.ds = ds;
 	}
 
 }
