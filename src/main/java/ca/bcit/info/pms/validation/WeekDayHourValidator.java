29b636d97dc8dc15925b98f1432efb9ad32b9b96
Huck5 <fcharleslundaahl@gmail.com>
2015-03-31 12:35:47 -0700

    validtionException not thrown...

--- src/main/java/ca/bcit/info/pms/validation/WeekDayHourValidator.java
+++ src/main/java/ca/bcit/info/pms/validation/WeekDayHourValidator.java
@@ -13,11 +13,8 @@ import org.apache.log4j.Logger;
 
 import ca.bcit.info.pms.controller.EditTimesheetController;
 
-@FacesValidator("ca.bcit.pms.util.WeekDayHourValidator")
+@FacesValidator("ca.bcit.pms.validation.WeekDayHourValidator")
 public class WeekDayHourValidator implements Validator {
-    @Inject
-    EditTimesheetController editTsController;
-
     private static final Logger logger = LogManager.getLogger(WorkPackageValidator.class);
 
     /**
@@ -29,17 +26,13 @@ public class WeekDayHourValidator implements Validator {
             UIComponent component, Object value) {
         FacesMessage msg;
         
-        try {
-            double hrs = (double) value;
-            if (hrs > 24.0) {
+        logger.info("\t\nWeekDayHourValidator.value::"+value);
+            if ((double) value > 24.0) {
+                logger.info("\t\nIf [value] > 24::value="+((double)value));
                 msg = new FacesMessage("Week-day hours invalid", "Must not be > 24");
                 msg.setSeverity(FacesMessage.SEVERITY_ERROR);
                 throw new ValidatorException(msg);
             }
-        } catch (Exception ex) {
-            msg = new FacesMessage("Week-day hours invalid","Not a valid decimal");
-            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(msg);
-        }
+        
     }
 }
