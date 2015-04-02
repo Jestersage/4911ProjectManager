ff725e5c2b4e2d04b30c3b462bd43386e23174df
rafaelrozon <rafaelrozon@gmail.com>
2015-04-01 18:48:30 -0700

    edit wp partially implemented. Need save method.

--- src/main/java/ca/bcit/info/pms/controller/WorkPackageController.java
+++ src/main/java/ca/bcit/info/pms/controller/WorkPackageController.java
@@ -14,6 +14,9 @@ import javax.inject.Named;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import ca.bcit.info.pms.access.EngineerBudgetManager;
+import ca.bcit.info.pms.access.WorkPackageManager;
+import ca.bcit.info.pms.model.EngineerBudget;
 import ca.bcit.info.pms.model.Project;
 import ca.bcit.info.pms.model.WorkPackage;
 import ca.bcit.info.pms.service.ProjectService;
@@ -24,135 +27,172 @@ import ca.bcit.info.pms.service.WorkPackageService;
  *
  */
 
-@Named("workPackageController")
+@Named( "workPackageController" )
 @RequestScoped
-public class WorkPackageController implements Serializable {
+public class WorkPackageController implements Serializable
+{
 
 	@Inject
 	private WorkPackage workPackage;
+
 	@Inject
 	private WorkPackageService workPackageService;
+
 	@Inject
 	private ProjectService projectService;
-	
-	private Project[] projectList;
-	
+
+	@Inject
+	private EngineerBudget engBudget;
+
+	@Inject
+	private EngineerBudgetManager engineerBudgetMngr;
+
+	@Inject
+	private WorkPackageManager wpMngr;
+
+	private Project [] projectList;
+
 	private Integer parentWPId;
 
-	private static final Logger logger = LogManager
-			.getLogger(WorkPackageController.class);
+	private static final Logger logger = LogManager.getLogger( WorkPackageController.class );
 
 	@Inject
 	private UserController userController;
 
-//	public String index() {
-//		if (workPackage == null)
-//			workPackage = new WorkPackage();
-//		if (workPackage.getProject() == null) {
-////			 FacesContext context = FacesContext.getCurrentInstance();
-////			 context.addMessage(null,
-////	                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
-////	                            "You haven't select a Project", null));	
-////			 return "";
-//			workPackage.setProject(new Project());
-//			projectList = getProjectList();
-//			   
-//		}
-//		
-//		logger.info("index new WorkPackage: ");
-//		return "newWorkPackage";
-//
-//	}
+	// public String index() {
+	// if (workPackage == null)
+	// workPackage = new WorkPackage();
+	// if (workPackage.getProject() == null) {
+	// // FacesContext context = FacesContext.getCurrentInstance();
+	// // context.addMessage(null,
+	// // new FacesMessage(FacesMessage.SEVERITY_ERROR,
+	// // "You haven't select a Project", null));
+	// // return "";
+	// workPackage.setProject(new Project());
+	// projectList = getProjectList();
+	//
+	// }
+	//
+	// logger.info("index new WorkPackage: ");
+	// return "newWorkPackage";
+	//
+	// }
 
-	
-	public Project[] getProjectList() {
-	    projectList = new Project[workPackageService.getAllProjects().size()];
-	    projectList =  workPackageService.getAllProjects().toArray(projectList);
-	    return projectList;
- 
+	public Project [] getProjectList()
+	{
+		projectList = new Project [ workPackageService.getAllProjects().size() ];
+		projectList = workPackageService.getAllProjects().toArray( projectList );
+		return projectList;
+
 	}
-	
-	
-	public String addWorkPackage() {
-		//System.out.println("budget=" + workPackage.getBudget().getP1());
+
+	public String addWorkPackage()
+	{
+		// System.out.println("budget=" + workPackage.getBudget().getP1());
 		WorkPackage parentWP = null;
-		logger.info(parentWPId);
-		if(parentWPId != null)
-			parentWP = workPackageService.findWorkPackageById(parentWPId);
-			workPackage.setParentWP(parentWP);
-		workPackageService.persistWorkPackage(workPackage);
-		//workPackageService.persistBudget(budget);
-		logger.info("successfully create new WorkPackage: "
-				+ workPackage.toString());
-		//System.out.println(workPackage.isLeaf());
+		logger.info( parentWPId );
+		if ( parentWPId != null )
+			parentWP = workPackageService.findWorkPackageById( parentWPId );
+		workPackage.setParentWP( parentWP );
+		workPackageService.persistWorkPackage( workPackage );
+		// workPackageService.persistBudget(budget);
+		logger.info( "successfully create new WorkPackage: " + workPackage.toString() );
+		// System.out.println(workPackage.isLeaf());
 		return "viewAllPackages";
 	}
 
-	public WorkPackage getworkPackage() {
-		if(workPackage.getParentWP() == null)
-			workPackage.setParentWP(new WorkPackage());
+	public WorkPackage getworkPackage()
+	{
+		if ( workPackage.getParentWP() == null )
+			workPackage.setParentWP( new WorkPackage() );
 		return workPackage;
 	}
 
-	public void setworkPackage(WorkPackage workPackage) {
+	public void setworkPackage( WorkPackage workPackage )
+	{
 		this.workPackage = workPackage;
 	}
 
-    public List<WorkPackage> getWorkPackages() {
-        return workPackageService.getAllWorkPackages();
-    }
-    
-    public String goEditPackage() {
-    	return "editWorkPackage";
-    }
-    
-    public String goNewTopLevelPackage(Project project) {
-    	workPackage.setProject(project);
-    	return "newWorkPackage";
-    }
-    
-    public String goChildPackage(WorkPackage parentWorkPackage) {
-    	Project project = projectService.getProject(parentWorkPackage.getProject().getId());    		
-    	workPackage.setProject(project);
-    	parentWPId = parentWorkPackage.getId();
-    	return "newWorkPackage";
-    }
+	public List< WorkPackage > getWorkPackages()
+	{
+		return workPackageService.getAllWorkPackages();
+	}
+
+	public String goEditPackage( final WorkPackage wp )
+	{
+		workPackage = wp;
+
+		// get engineer budget and assign to property
+		int engBudgetID = wpMngr.getEngBudgetID( wp.getId() );
+		engBudget = engineerBudgetMngr.find( engBudgetID );
+
+		return "editWP";
+	}
+
+	public String goNewTopLevelPackage( Project project )
+	{
+		workPackage.setProject( project );
+		return "newWorkPackage";
+	}
+
+	public String goChildPackage( WorkPackage parentWorkPackage )
+	{
+		Project project = projectService.getProject( parentWorkPackage.getProject().getId() );
+		workPackage.setProject( project );
+		parentWPId = parentWorkPackage.getId();
+		return "newWorkPackage";
+	}
 
 	/**
 	 * @return a list of all work packages an employee is assigned to.
 	 */
-	public List<WorkPackage> getMyWorkPackages() {
+	public List< WorkPackage > getMyWorkPackages()
+	{
 		final String userId = userController.getUser().getId();
-		return workPackageService.findAssignedWorkPackages(userId);
+		return workPackageService.findAssignedWorkPackages( userId );
 	}
 
-
-	public Integer getParentWPId() {
+	public Integer getParentWPId()
+	{
 		return parentWPId;
 	}
 
-
-	public void setParentWPId(Integer parentWPId) {
+	public void setParentWPId( Integer parentWPId )
+	{
 		this.parentWPId = parentWPId;
 	}
 
-    public void setBooleanLeaf (AjaxBehaviorEvent event) {   	
-    	workPackage.setLeaf(workPackage.isLeaf());
-    }
-    
-    /**
-     * 
-     * @return
-     */
-    public String viewPackageDetails(final WorkPackage workPackage){
-    	this.workPackage = workPackage;
-    	return "viewWorkPackageDetails";
-    }
+	public void setBooleanLeaf( AjaxBehaviorEvent event )
+	{
+		workPackage.setLeaf( workPackage.isLeaf() );
+	}
 
-	public String goCreatePackage(final String projId) {
-		final Project project = projectService.getProject(projId);
-		workPackage.setProject(project);
+	/**
+	 * 
+	 * @return
+	 */
+	public String viewPackageDetails( final WorkPackage workPackage )
+	{
+		this.workPackage = workPackage;
+		return "viewWorkPackageDetails";
+	}
+
+	public String goCreatePackage( final String projId )
+	{
+		final Project project = projectService.getProject( projId );
+		workPackage.setProject( project );
 
 		return "newWorkPackage";
 	}
+
+	public EngineerBudget getEngBudget()
+	{
+		return engBudget;
+	}
+
+	public void setEngBudget( EngineerBudget engBudget )
+	{
+		this.engBudget = engBudget;
+	}
+
 }
